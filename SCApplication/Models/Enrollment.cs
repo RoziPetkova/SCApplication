c92a91216ec21326793e23fc4bae914b7478f5fc
RoziPetkova <savana_93@abv.bg>
2018-03-14 14:47:13 +0200

    first commit Signed-off-by: RoziPetkova <savana_93@abv.bg>

---
+++ SCApplication/Models/Enrollment.cs
@@ -0,0 +1,12 @@
+﻿namespace SCApplication.Models
+{
+    public class Enrollment
+    {
+        public int ID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
