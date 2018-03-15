f66e1d7bd072bcc56b9f8c8c23e72e51c40e2663
RoziPetkova <savana_93@abv.bg>
2018-03-15 17:57:01 +0200

    Update Migrations
    Signed-off-by: RoziPetkova <savana_93@abv.bg>

--- SCApplication/Models/Course.cs
+++ SCApplication/Models/Course.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SCApplication.Models
 {
     public class Course
     {
+        [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseID { get; set; }
+        [Required]
         public string Title { get; set; }
         public int Credits { get; set; }
 
--- SCApplication/Models/Enrollment.cs
+++ SCApplication/Models/Enrollment.cs
@@ -1,9 +1,13 @@
-﻿namespace SCApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SCApplication.Models
 {
     public class Enrollment
     {
         public int ID { get; set; }
+        [Required]
         public int CourseID { get; set; }
+        [Required]
         public int StudentID { get; set; }
 
         public virtual Course Course { get; set; }
